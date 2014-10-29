0d2e6966fa82d9ff788793844e7505715d88cb0f
Angie <angienguyen629@gmail.com>
2014-10-29 13:29:57 -0700

    form stuff

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/email', function(req, res) {
+  res.send(req.body.name + ' ' + req.body.email + ' ' + req.body.subject);
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 8000);
 
-console.log('Server running on port 80');
+console.log('Server is now running');
