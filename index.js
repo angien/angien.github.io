c2b8008002af93fb8cdd662d3951f112d0359469
Angie <angienguyen629@gmail.com>
2014-10-29 13:43:55 -0700

    added vimrc stuff

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+
+// for form stuff
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/email', function(req, res) {
   res.send(req.body.name + ' ' + req.body.email + ' ' + req.body.subject);
 });
 
+// for serving public folder
 app.use(express.static(__dirname + '/public'));
 
+// choose a port
 app.listen(process.env.PORT || 8000);
 
 console.log('Server is now running');
