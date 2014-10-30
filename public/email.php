6b6fdd907c0543786cd8f4fd0d0fd0451314bcfc
Angie <angienguyen629@gmail.com>
2014-10-29 17:30:37 -0700

    contact stuff

--- public/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-phpinfo();
-
-
-	$headers .= 'From:' . $_POST['name'] . " <" . $_POST['email'] . ">" . "\r\n";
-    mail('angienguyen@gmail.com.com', $_POST['subject'], $_POST['message'], $headers);
-?>
-<p>Your email has been sent.</p>
